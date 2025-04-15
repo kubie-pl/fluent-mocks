c36941fee56a3b80c0166f2cee01fc52adc91dc5
Jakub Bieda <bieda.kuba@gmail.com>
2025-04-15 14:38:07 +0200

    initial commit

---
+++ src/main/java/pl/kubie/fluent_mocks/FluentMocksApplication.java
@@ -0,0 +1,13 @@
+package pl.kubie.fluent_mocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluentMocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluentMocksApplication.class, args);
+	}
+
+}
