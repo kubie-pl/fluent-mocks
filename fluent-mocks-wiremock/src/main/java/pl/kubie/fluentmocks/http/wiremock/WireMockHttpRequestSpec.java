2d3a6c525d6a14a1648d26edcb8348c2861663c9
Jakub Bieda <bieda.kuba@gmail.com>
2025-09-16 14:39:09 +0200

    fix: fix matching all values for headers and query params in WireMock

--- fluent-mocks-wiremock/src/main/java/pl/kubie/fluentmocks/http/wiremock/WireMockHttpRequestSpec.java
+++ fluent-mocks-wiremock/src/main/java/pl/kubie/fluentmocks/http/wiremock/WireMockHttpRequestSpec.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2025 the original author or authors
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2025 the original author or authors
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package pl.kubie.fluentmocks.http.wiremock;
 
@@ -107,7 +107,10 @@ public class WireMockHttpRequestSpec implements MockHttpRequestSpec {
     return pattern;
   }
 
-  private static @NotNull ExactMatchMultiValuePattern exactMatch(List<String> values, Function<String, StringValuePattern> patternFactory) {
+  private static @NotNull ExactMatchMultiValuePattern exactMatch(
+      List<String> values,
+      Function<String, StringValuePattern> patternFactory
+  ) {
     return values.stream()
         .map(patternFactory)
         .collect(collectingAndThen(toList(), ExactMatchMultiValuePattern::new));
